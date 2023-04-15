cd38112285966f1d7a1506c53c4804b9d14d32f4
MoaraMiranda <moaramirandass@gmail.com>
2023-04-15 18:41:32 +0200

    Added week Forecast data

--- src/components/WeekForecast.jsx
+++ src/components/WeekForecast.jsx
@@ -1,7 +1,17 @@
 import "../WeekForecast.css"
 
 export default function WeekForecast(){
+  
   return (
-    <div id="forecast" className="WeekForecast row row-cols-5 text-center forecast"></div>
+    <div className="WeekForecast row row-cols-5 text-center forecast">
+      <div className="col">
+        <div className="weather-forecast-day">Mon</div>
+        <img src="./public/sunny.png" alt="icon" height={90} />
+        <div>
+          <span>19°</span>
+          <span className="weather-forecast-temperature-min"> 10°</span>
+        </div>
+      </div>
+    </div>
   );
 }
