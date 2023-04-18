ec73629740c34d59190357c2b195f38d79d6ced8
MoaraMiranda <moaramirandass@gmail.com>
2023-04-18 15:31:36 +0200

    replace http icon to fix mobile

--- src/components/ForecastDay.jsx
+++ src/components/ForecastDay.jsx
@@ -5,14 +5,11 @@ export default function ForecastDay(props) {
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return days[day];
   }
-
+ 
   return (
     <>
       <div className="weather-forecast-day">{day()}</div>
-      <img
-        src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-        alt="icon"
-      />
+      <img src={props.data.condition.icon_url.replace("http", "https")} alt="icon" />
       <div className="weather-forecast-temp">
         <span>{Math.round(props.data.temperature.maximum)}°</span>
         <span className="weather-forecast-temperature-min">
